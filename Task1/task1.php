ed4546fb1eb749e80e571d79235847b929a7e13f
Samir Faisal <82781006+SamirFaisal20@users.noreply.github.com>
2022-01-31 17:24:39 +0600

    Add files via upload

---
+++ Task1/task1.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <h2><b>Registration form</b></h2>
+        <hr>
+        <form action="">
+            <table>
+                <tr>
+                    <td>First Name:</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td>Last Name:</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td>Age:</td>
+                    <td><input type="number"></td>
+                </tr>
+                <tr>
+                    <td>Designation:</td>
+                    <td><input type="radio" name="desination">Junior Programmer
+                    <input type="radio" name="desination">Senior Programmer
+                    <input type="radio" name="desination">Project Lead
+                    </td>
+                </tr>
+                <tr>
+                    <td>Peferred language:</td>
+                    <td><input type="checkbox" name="language">JAVA
+                    <input type="checkbox" name="language">PHP
+                    <input type="checkbox" name="language">C++
+                    </td>
+                </tr>
+                <tr>
+                    <td>E-mail:</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type="password"></td>
+                </tr>
+                <tr>
+                    <td>Please choose a file</td>
+                    <td><input type="file"></td>
+                </tr>
+            </table>
+                <input type="submit" value="Submit">
+                <input type="submit" value="Reset">
+        </form>
+        
+    </body>
+
+</html>
